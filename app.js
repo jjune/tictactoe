714d4c2ce9df87542bebd8c63c2372f52b4618dd
Joseph June <joseph.june@gmail.com>
2015-08-05 09:03:46 -0400

    handles tied game

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
 var currentPlayer = 'x';
 var x_score = 0;
 var o_score = 0;
+var turns = 0;
 var win_combinations = [7, 56, 448, 73, 146, 292, 273, 84];
 
 // Alternate players and draw shape
 $('#board').on('click', '.cell', function() {
 	var $current_cell = $(event.target);
 	$current_cell.addClass('cell-' + currentPlayer);
+    turns++;
 
 	// Keep track of each player's score
     window[currentPlayer + '_score'] +=  parseInt(event.target.id);
@@ -18,10 +20,15 @@ $('#board').on('click', '.cell', function() {
             if ((win_combinations[i] & window[currentPlayer + '_score']) === win_combinations[i]) {
                 alert(currentPlayer + " wins!");
                 location.reload();
+                return false;
             }
         };
 
-    // Swap player
-	currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
-
+    if (turns > 8) {
+        alert("It's a Tie!");
+        location.reload();
+        return false;
+    }
+        // Swap player
+    currentPlayer = currentPlayer === 'x' ? 'o' : 'x';
 });
